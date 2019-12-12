17c6ee573e24f4ad5c622ef42a61404b4d53c1a0
kishore.7697 <kishore.7697@gmail.com>
2019-12-12 19:39:06 +0530

    i have done my work

--- src/test/java/Cloning.java
+++ src/test/java/Cloning.java
@@ -1,7 +1,9 @@
 
 public class Cloning {
 	public static void main(String[] args) {
-		System.out.println("ok");
+	System.out.println("ok");
+	System.out.println("hello");
+	System.out.println("branch coding is done");
 	}
 
 }
